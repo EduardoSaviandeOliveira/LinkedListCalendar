ed56908b1486901a5fae12d48e052a31f2e5cf52
Eduardo Savian de Oliveira <eduardosaviandeoliveira@proton.me>
2023-04-12 23:12:03 -0300

    Added menu

--- main.cpp
+++ main.cpp
@@ -2,7 +2,71 @@
 #include "Commitment/commitment.hpp"
 
 int main() {
-    CalendarList *caledar = CreateCaledar();
+    CalendarList *calendar = CreateCaledar();
+    std::string command;
+    while(true) {
+        std::cout << "Selecione uma opção:\n";
+        std::cout << "1 - Inserir dia\n";
+        std::cout << "2 - Remover dia\n";
+        std::cout << "3 - Inserir compromisso\n";
+        std::cout << "4 - Remover compromisso\n";
+        std::cout << "5 - Imprimir calendário\n";
+        std::cout << "6 - Imprimir compromissos\n";
+        std::cout << "7 - Sair\n";
+        std::cout << "Opção: ";
+        std::cin >> command;
+        
+        if (command == "1") {
+            std::string date;
 
+            std::cout << "Adicionar - Digite a data (aaaa-MM-dd): ";
+            std::cin >> date;
+
+            InsertDate(calendar, date);
+        } else if (command == "2") {
+            std::string date;
+
+            std::cout << "Remover - Digite a data (aaaa-MM-dd): ";
+            std::cin >> date;
+
+            RemoveDate(calendar, date);
+        } else if (command == "3") {
+            std::string date;
+            std::string startTime;
+            std::string endTime;
+            std::string description;
+
+            std::cout << "Inserir compromisso - Digite a data (aaaa-MM-dd): ";
+            std::cin >> date;
+            std::cout << "Inserir compromisso - Digite o horário de início (hh:mm): ";
+            std::cin >> startTime;
+            std::cout << "Inserir compromisso - Digite o horário de término (hh:mm): ";
+            std::cin >> endTime;
+            std::cout << "Inserir compromisso - Digite a descrição: ";
+            std::cin >> description;
+
+            InsertCommitment(date, calendar, startTime, endTime, description);
+        } else if (command == "4") {
+            std::string date;
+            std::string startTime;
+
+            std::cout << "Inserir compromisso - Digite a data (aaaa-MM-dd): ";
+            std::cin >> date;
+            std::cout << "Inserir compromisso - Digite o horário de início (hh:mm): ";
+            std::cin >> startTime;
+
+            RemoveCommitment(date, calendar, startTime);
+        } else if (command == "5") {
+            PrintCalendar(calendar);
+        } else if (command == "6") {
+            std::string date;
+
+            std::cout << "Imprimir compromissos - Digite a data (aaaa-MM-dd): ";
+
+            PrintCommitments(calendar, date);
+        } else if (command == "7") {
+            break;
+        }
+    }
     return 0;
 }
